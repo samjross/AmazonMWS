6b2dd4bbd3267059fd88418bbde3d9dcd33ff86c
Kremlin <sjoshuaross@yahoo.com>
2014-06-25 17:12:31 +0100

    returns settings object rather than settings dictionary

--- jSonCredentials.py
+++ jSonCredentials.py
@@ -1,14 +1,20 @@
 import json
 
+
+class Struct:
+    def __init__(self, **entries):
+        self.__dict__.update(entries)
+
+
 def load_settings():
     credentials = str(open("credentials.json").read())
-    print(credentials)
-    settings_object = json.loads(credentials, encoding="ASCII")
-    ret_dict = {}
-    for key, val in settings_object.items():
+    settings_dict = json.loads(credentials, encoding="ASCII")
+    str_dict = {}
+    for key, val in settings_dict.items():
         if type(key) == unicode:
             key = str(key)
         if type(val) == unicode:
             val = str(val)
-        ret_dict[key] = val
-    return ret_dict
+        str_dict[key] = val
+    settings_object = Struct(**str_dict)
+    return settings_object
--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from jSonCredentials import load_settings
 
 settings = load_settings()
 
-api = mws.Inventory(account_id=settings['AccountID'], secret_key=settings['SecretKey'],
-                    access_key=settings['AccessKey'], region='UK')
+api = mws.Inventory(account_id=settings.AccountID, secret_key=settings.SecretKey,
+                    access_key=settings.AccessKey, region='UK')
 
 result = api.list_inventory_supply(skus=('196267', '196268rb'))
 pass
