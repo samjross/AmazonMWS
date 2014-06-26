5638f0ba4845a4469b5a4c526a68f987a6d5572c
Kremlin <sjoshuaross@yahoo.com>
2014-06-26 22:11:44 +0100

    get flat file MWS request

--- main.py
+++ main.py
@@ -3,7 +3,13 @@ from jSonCredentials import Credentials
 
 credentials = Credentials()
 
-api = mws.Inventory(account_id=credentials.AccountID, secret_key=credentials.SecretKey,
-                    access_key=credentials.AccessKey, region='UK')
+# api = mws.Inventory(account_id=credentials.AccountID, secret_key=credentials.SecretKey,
+# access_key=credentials.AccessKey, region='UK')
+#
+# result = api.list_inventory_supply(skus=('FBAFBA_110919', '196268rb'))
 
-result = api.list_inventory_supply(skus=('196267', '196268rb'))
+api = mws.Reports(access_key=credentials.AccessKey, secret_key=credentials.SecretKey,
+                  account_id=credentials.AccountID, region='UK')
+
+x = api.request_report(report_type='_GET_FLAT_FILE_OPEN_LISTINGS_DATA_',
+                       marketplaceids=(credentials.MarketplaceID,))
