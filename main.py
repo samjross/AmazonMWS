3938931956210d6d87064fa59dbfb41c66a603dc
Kremlin <sjoshuaross@yahoo.com>
2014-06-27 22:19:27 +0100

    get_report_request_list

--- main.py
+++ main.py
@@ -11,5 +11,11 @@ credentials = Credentials()
 api = mws.Reports(access_key=credentials.AccessKey, secret_key=credentials.SecretKey,
                   account_id=credentials.AccountID, region='UK')
 
-x = api.request_report(report_type='_GET_FLAT_FILE_OPEN_LISTINGS_DATA_',
-                       marketplaceids=(credentials.MarketplaceID,))
+# request = api.request_report(report_type='_GET_FLAT_FILE_OPEN_LISTINGS_DATA_',
+#                      marketplaceids=(credentials.MarketplaceID,))
+
+request_list = api.get_report_request_list()
+# what I'm going to need to do is, when I make a request I need to store the report ID.
+# Then, I check every now and then if that report ID is 'done'
+# if it is, use api.get_report(reportID) to get the report
+# will see what happens after that...
