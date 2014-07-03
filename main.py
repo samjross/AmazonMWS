ae02fc82aa96f898e7e26c4d722e0af47bda02a1
Kremlin <sjoshuaross@yahoo.com>
2014-07-03 18:54:27 +0100

    refactored to use dictionaries for listings

--- main.py
+++ main.py
@@ -22,15 +22,25 @@ api = mws.Reports(access_key=credentials.AccessKey, secret_key=credentials.Secre
 def request_wait_get_and_parse_flat_file():
     """
     _GET_FLAT_FILE_OPEN_LISTINGS_DATA_
-    returns a list of lists of listings
-    each sublist contains 4 values:
-    sku, asin, price, quantity
-    [
-    [01100, 2067166263, 4.49, 5],
-    [01325, 1906261776, 4.49, 22]
-    ]
+    returns a list of dictionaries of listins
+    each dictionary is made of the following keys:
+
+    'sku', 'asin', 'price', 'quantity'
+    -----------------------------------------------------------------------------------
 
     alternatively, _GET_MERCHANT_LISTINGS_DATA_ returns a much bigger list of properties
+    here are the keys:
+
+    'zshop-boldface', 'pending-quantity', 'seller-sku', 'zshop-browse-path',
+    'item-condition', 'product-id', 'asin2', 'asin3', 'asin1', 'image-url',
+    'item-description', 'product-id-type', 'open-date', 'will-ship-internationally',
+    'listing-id', 'price', 'add-delete', 'zshop-storefront-feature', 'item-note',
+    'item-name', 'zshop-category1', 'expedited-shipping', 'zshop-shipping-fee',
+    'bid-for-featured-placement', 'item-is-marketplace', 'fulfillment-channel', 'quantity'
+
+    The important ones:
+    'seller-sku', 'quantity', 'price', 'product-id' = 'asin1',
+    'item-name', 'item-condition', 'fulfillment-channel'
     """
     request = api.request_report(report_type='_GET_MERCHANT_LISTINGS_DATA_',
                                  marketplaceids=(credentials.MarketplaceID,))
@@ -65,12 +75,17 @@ def get_and_parse_report(generated_report_id):
     temp_file.close()
     temp_file = open('report.txt', 'r')
     csv_reader = csv.reader(temp_file, dialect=AmazonCSVDialect)
-    #csv_reader.next()
-    listings = [line for line in csv_reader]
+    keys = csv_reader.next()
+    listings = []
+    for line in csv_reader:
+        listing_dict = {}
+        for index in range(len(line)):
+            listing_dict[keys[index]] = line[index]
+        listings.append(listing_dict)
     temp_file.close()
     return listings
 
 
-listings = request_wait_get_and_parse_flat_file()
-for listing in listings:
+listins = request_wait_get_and_parse_flat_file()
+for listing in listins:
     print listing
